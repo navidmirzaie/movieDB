5b3aec2dfe2e2df21fa93f1edf2d721f7ffbec9f
navidmirzaie <navids.inbox@gmail.com>
2021-10-11 11:48:30 +0200

    changing http to https

--- src/api.js
+++ src/api.js
@@ -2,7 +2,6 @@
 
 const API_KEY       = `4ffb1f9c`;
 const URL_ENDPOINT  = `https://www.omdbapi.com/`;
-
 /** 
 
 * function for searching movie
@@ -23,7 +22,6 @@ export function getMovie(query, param) {
     } else if(param === 'search') {
         param = 's';
     }
-    console.log(`${URL_ENDPOINT}?${param}=${query}&apikey=${API_KEY}`);
     const request = fetch(`${URL_ENDPOINT}?${param}=${query}&apikey=${API_KEY}`)
     .then(response => response.json())
 
